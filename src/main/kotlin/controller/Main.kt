e18a19dd9b765e999143d649ee7f5cae48cab4f6
jesus.caballero.7e7 <jesus.caballero.7e7@itb.cat>
2024-03-22 12:19:56 +0100

    Implementando menú

--- src/main/kotlin/controller/Main.kt
+++ src/main/kotlin/controller/Main.kt
@@ -2,6 +2,7 @@ package org.example.controller
 
 import org.example.GREEN_BOLD
 import org.example.RESET
+import org.example.readInt
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
@@ -11,6 +12,35 @@ val url = "jdbc:postgresql://surus.db.elephantsql.com/"
 val usuario = "mkhwfgyy"
 val password = "GHT70ECP3mC7HHFQ5zAH3yt6UnuJLyOj"
 
+
+fun menu(){
+    var dato=false
+    println("Bienvenido a la tienda de portátiles más extensa del mundo! undo! ndo! do! o! !")
+    do {
+        println(
+            "Escoja una de las siguientes opciones: " +
+                    "1. Visualizar los datos.\n" +
+                    "2. Buscar datos por campos.\n" +
+                    "3. Escribe tu propia consulta. Además podrás generar un informe con el que compartir con tus seres queridos :D.\n" +
+                    "4. Pulse esta opción si desear modificar algún dato de esta base de datos.\n" +
+                    "5. Si desea salir, marque esta opción"
+        )
+        var opcion= readInt("Marque ahora su opción a elegir","Tiene que ser un número")
+        when(opcion){
+            1-> println("Ahora puede ver los datos según estos criterios: \n" +
+                    "1. Visualizar por marca\n" +
+                    "2. Visualizar por precio\n" +
+                    "3. Visualizar por puntuación")
+            2-> println("Ahora puede filtrar por los siguientes campos: \n" +
+                    "1. Por marca.\n" +
+                    "2. Por precio máximo.\n" +
+                    "3. Por puntuación mínima.\n" +
+                    "4. Por número mínimo de cores.\n" +
+                    "5. Por cantidad mínima de memoria.\n" +
+                    "6. Por pulgadas de pantalla")
+        }
+    }while (!dato)
+}
 fun main() {
     var dbConnection: Connection? = conectarBBDD(url, usuario, password)
     var query: String
